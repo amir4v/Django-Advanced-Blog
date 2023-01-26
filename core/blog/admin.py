2c64b5f73dc9af4cbf90fe7bda3c2f897f195705
Amir <84547283+amir4v@users.noreply.github.com>
2023-01-26 12:00:49 +0330

    permreqmixin

---
+++ core/blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Category, Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'status', 'category', 'created_dt', 'published_dt']
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostModelAdmin)
